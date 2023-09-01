095f2d4d089fa6a68f3ccc9d4e3c1ba01bc909b5
davinci011 <135906162+davinci011@users.noreply.github.com>
2023-09-01 08:49:40 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let count = 0 
+let countEl = document.getElementById("count-el")
+let saveEl =  document.getElementById("save-el")
+
+function step() { 
+    count += 1
+    countEl.innerText = count
+}
+
+function save(){
+    let render = " " +  count + " - " 
+    saveEl.textContent +=   render
+    countEl.textContent = 0
+    count = 0
+  
+}
+
+   
+
+
